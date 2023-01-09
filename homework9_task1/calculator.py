3140d0091c70afd67d1049c0a2bc0871e18228a8
PavelSB <1987PavelS@mail.ru>
2023-01-09 19:41:27 +0300

    home work 9 task 1 done

--- homework9_task1/calculator.py
+++ homework9_task1/calculator.py
@@ -1,1 +1,22 @@
-#
+# простой калькулятор, созданный в виртуальном окружении
+
+numA = int(input("Enter number A: "))
+sing = input("Enter operation sing: ")
+numB = int(input("Enter number B: "))
+
+def calc(a, s, b):
+    result = 0
+    if s == "+":
+        result = a + b
+    if s == "-":
+        result = a - b
+    if s == "*":
+        result = a * b
+    if s == "/":
+        result = a / b
+    if s == "^":
+        result = a ** b
+    return result
+
+result = calc(numA, sing, numB)
+print(f"{numA} {sing} {numB} = {result}")
