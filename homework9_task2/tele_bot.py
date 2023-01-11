ea4d24606a4d6196c2924a41426d5c11b8d26a45
PavelSB <1987PavelS@mail.ru>
2023-01-11 22:32:14 +0300

    home work 9 task 2 done

--- homework9_task2/tele_bot.py
+++ homework9_task2/tele_bot.py
@@ -1,27 +1,30 @@
 import time
+import datetime
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
+logging.basicConfig(level=logging.INFO)
+
 with open("info.txt", "r") as f:
     text = f.readline()
-TOKEN = text
-MSG = "{}, how are you feeling today?"
+TOKEN = str(text)
+MSG = "{}, how are you feeling today? ({}, {})"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
-
 
 @dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     user_full_name = message.from_user.full_name
-    logging.INFO(f'{user_id=} {user_full_name=} {time.asctime()}')
+    logging.info(f'{user_id=} {user_full_name=} {time.asctime()}')
     await message.reply(f"Hi, {user_full_name}!")
 
-    for i in range(10):
-        time.sleep(2)
-        await bot.send_message(user_id, MSG.format(user_name))
+    for i in range(7):
+        time.sleep(60*60*24)
+        k = i + 1
+        await bot.send_message(user_id, MSG.format(user_name, datetime.datetime.now().strftime('%a'), k))
 
 if __name__ == '__main__':
     executor.start_polling(dp)
